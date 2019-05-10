4ad019b8e1806d048229f7f965ee82ac9a2414c0
TatuDeBoa <bruno.19ls@gmail.com>
2019-05-10 15:35:01 -0300

    test...

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
 const port = process.env.PORT || "3000";
 
-const server = http.createServer(function(req, res) {
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
 });
 
-server.listen(port, hostname, function() {
-  console.log('Server running at http://'+ hostname + ':' + port + '/');
-})
+server.listen(port);
